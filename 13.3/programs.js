cab814081d92ea5b45a1b2f0ecdcfa21dcaf2c04
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-02-13 20:31:23 +0100

    modifying line 13

--- 13.3/programs.js
+++ 13.3/programs.js
@@ -1,6 +1,7 @@
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
     var input = process.stdin.read();
+    var node = node.version;
     if (input !== null) {
         var instruction = input.toString().trim();
         switch (instruction) {
@@ -9,7 +10,7 @@ process.stdin.on('readable', function() {
             process.exit();
         break;
         case '/node':
-            process.stdout.write(node.version)
+            process.stdout.write('Node wersion:' node)
         	break;
         default:
         	process.stdout.write('Wrong instruction!\n');
