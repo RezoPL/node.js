9f83fab72cca8553895de46387de7bab5b5202c3
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-02-13 20:26:54 +0100

    add default instruction

--- 13.3/programs.js
+++ 13.3/programs.js
@@ -3,14 +3,16 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        case (instruction === '/exit') {
+        switch (instruction) {
+        case '/exit':
             process.stdout.write('Quitting app!\n');
             process.exit();
-        }
         break;
-        case (instruction === 'node') {
+        case '/node':
             process.stdout.write(node.version)
+        	break;
+        default:
+        	process.stdout.write('Wrong instruction!\n');
         }
-        break;
     }
 });
