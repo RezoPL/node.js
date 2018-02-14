89dc7aae31e0e100162fc64df2f2a2faba9ac823
Przemek Jurkiewicz <hankusekbb@gmail.com>
2018-02-14 18:24:11 +0100

    install formidable module + change handlers welcom&upload section

--- 13.8.Project/modules/server.js
+++ 13.8.Project/modules/server.js
@@ -4,7 +4,7 @@ var colors = require('colors');
 function start() {
   function onRequest(request, response) {
     console.log("Odebrano zapytanie.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
     response.write("Pierwsze koty za płoty");
     response.end();
   }
